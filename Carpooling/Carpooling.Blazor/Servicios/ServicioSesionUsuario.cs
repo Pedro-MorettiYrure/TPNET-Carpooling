86553175d706968af3ccf87c692705aa87fb9baf
candegarcia <candegarciamo@gmail.com>
2025-10-20 11:53:33 -0300

    agrega blazor con login registrar ser conductor y vehiculos

---
+++ Carpooling/Carpooling.Blazor/Servicios/ServicioSesionUsuario.cs
@@ -0,0 +1,24 @@
+﻿using DTOs;
+
+public class ServicioSesionUsuario
+{
+    public UsuarioDTO UsuarioActual { get; private set; }
+
+    public event Action OnChange;
+
+    public void IniciarSesion(UsuarioDTO usuario)
+    {
+        UsuarioActual = usuario;
+        NotifyStateChanged();
+    }
+
+    public void CerrarSesion()
+    {
+        UsuarioActual = null;
+        NotifyStateChanged();
+    }
+
+    public bool EstaLogueado => UsuarioActual != null;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
